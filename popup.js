4b8d36c45e5dba37dec14d4efe0f3f1e882c83f2
Veronez1 <daviveronez8@gmail.com>
2024-06-13 12:33:58 -0300

    Funcionalidades

---
+++ popup.js
@@ -0,0 +1,12 @@
+console.log("Teste");
+
+var clique = document.getElementById("primeiro");
+
+var mode = document.getElementById("darkmode");
+
+mode.onclick = function () {
+    clique.style.backgroundColor = "grey";
+}
+
+
+//clique.style.color = "red";
